b46d8cc3f322b3b12ff0150f6c6ee770f407d2a6
Rich Hildebrand <Rich@Clear-Measure.com>
2014-08-02 18:01:44 -0400

    cannot poll 1 on 1 messages

--- HipChatWPF/MainWindow.xaml.cs
+++ HipChatWPF/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System.Linq;
+﻿using System.Configuration;
+using System.Linq;
+using System.Net;
 using System.Windows;
 using HipchatApiV2;
 using HipChatWPF.Model;
+using System.Windows.Input;
 
 namespace HipChatWPF
 {
@@ -17,6 +20,14 @@ namespace HipChatWPF
             _client = new HipchatClient();
             InitializeComponent();
             PopulateUsers();
+            PopulateRooms();
+        }
+
+        private void PopulateRooms()
+        {
+            var roomsResponse = _client.GetAllRooms();
+            var rooms = roomsResponse.Items.Select(room => room.Name).ToList();
+            Rooms.ItemsSource = rooms;
         }
 
         private void PopulateUsers()
@@ -24,6 +35,23 @@ namespace HipChatWPF
             var usersResponse = _client.GetAllUsers();
             var users = usersResponse.Items.Select(user => new User(user)).ToList();
             Users.ItemsSource = users;
+        }
+
+        private void SendMessage_OnKeyUp(object sender, KeyEventArgs e)
+        {
+            var accessToken = ConfigurationManager.AppSettings["hipchat_auth_token"];
+            var Id = "919511"; //me
+            if (e.Key != Key.Enter)
+            {
+                return; 
+            }
+
+            var message = @"https://api.hipchat.com/v2/user/" + Id + "/" + "Hello";
+
+            WebRequest request = WebRequest.Create(message);
+            request.Headers.Add("Authorization", accessToken);
+            request.Method="Post";
+            request.GetResponse();
         }
     }
 }
