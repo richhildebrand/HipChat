c9b385d3ee6f7b1d0a578f550f0a51af73251ab5
Rich Hildebrand <Rich@Clear-Measure.com>
2014-08-02 11:35:20 -0400

    pull names from server

--- HipChatWPF/MainWindow.xaml.cs
+++ HipChatWPF/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
+using HipchatApiV2;
+using HipChatWPF.Model;
 
 namespace HipChatWPF
 {
@@ -7,9 +10,20 @@ namespace HipChatWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private HipchatClient _client;
+
         public MainWindow()
         {
+            _client = new HipchatClient();
             InitializeComponent();
+            PopulateUsers();
+        }
+
+        private void PopulateUsers()
+        {
+            var usersResponse = _client.GetAllUsers();
+            var users = usersResponse.Items.Select(user => new User(user)).ToList();
+            Users.ItemsSource = users;
         }
     }
 }
---
+++ HipChatWPF/Model/User.cs
@@ -0,0 +1,19 @@
+﻿using HipchatApiV2.Responses;
+
+namespace HipChatWPF.Model
+{
+    public class User
+    {
+        private readonly HipchatUser _user;
+
+        public User(HipchatUser user)
+        {
+            _user = user;
+        }
+
+        public override string ToString()
+        {
+            return _user.Name ;
+        }
+    }
+}
