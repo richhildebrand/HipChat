0adbfcf90f004c54404acb53ebf5d5e6357c9e9f
Rich Hildebrand <Rich@Clear-Measure.com>
2014-08-16 10:30:10 -0400

    list rooms with checkbox

--- HipChatWPF/MainWindow.xaml.cs
+++ HipChatWPF/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System.Configuration;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Windows;
 using HipchatApiV2;
+using HipchatApiV2.Responses;
 using HipChatWPF.Model;
 using System.Windows.Input;
 
@@ -25,8 +28,15 @@ namespace HipChatWPF
         private void PopulateRooms()
         {
             var roomsResponse = _client.GetAllRooms();
-            var rooms = roomsResponse.Items.Select(room => room.Name).ToList();
-            Rooms.ItemsSource = rooms;
+            var rooms = roomsResponse.Items.ToList();
+            var observableRooms = new ObservableCollection<HipchatGetAllRoomsResponseItems>();
+
+            foreach (var room in rooms)
+            {
+                observableRooms.Add(room);
+            }
+
+            Rooms.ItemsSource = observableRooms;
         }
     }
 }
